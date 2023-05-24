78ee6fc330ce9009b5cb257667095a40ee81d1b9
thordal <thordal@simetzberger.at>
2023-05-24 16:43:28 +0200

    basic server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+// Serve static content in directory 'files'
+app.use(express.static(path.join(__dirname, 'files')));
+
+
+app.listen(3000)
+
+console.log("Server now listening on http://localhost:3000/")
