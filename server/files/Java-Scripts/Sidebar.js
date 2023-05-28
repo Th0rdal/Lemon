74aaf402652ea0e4884e51c5564ed129277fb1ec
Lukas110596 <schertlerlukas96@gmail.com>
2023-05-28 17:05:06 +0200

    Wiederhestellung

--- server/files/Java-Scripts/Sidebar.js
+++ server/files/Java-Scripts/Sidebar.js
@@ -1,12 +1,4 @@
-
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("collapsed");
 }
-
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
-
