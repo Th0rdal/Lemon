287f51ffeb8071ae8d639b2d419ed4b391192c3a
Lukas110596 <schertlerlukas96@gmail.com>
2023-05-28 17:21:36 +0200

    Playing around and try to style the first page

--- server/files/Java-Scripts/Sidebar.js
+++ server/files/Java-Scripts/Sidebar.js
@@ -2,3 +2,10 @@ function toggleSidebar() {
     const sidebar = document.getElementById("sidebar");
     sidebar.classList.toggle("collapsed");
 }
+
+var button = document.getElementById("VegiButton");
+var options = document.getElementById("VegiOptions");
+
+button.addEventListener("click", function() {
+    options.style.display = options.style.display === "none" ? "block" : "";
+});
