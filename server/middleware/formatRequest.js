73435ee245a3258a4dbd0e381dd2fe2d44eb4ef4
patrick <thordal.simetzberger@gmail.com>
2023-05-30 15:00:39 +0200

    parseXML now only transforms body to json if the content-type is application/xml

--- server/middleware/formatRequest.js
+++ server/middleware/formatRequest.js
@@ -4,6 +4,10 @@ function parseXML(req, res, next) {
     takes the preparsed xml and converts it into the same data format as json
     send 400: if there was an error with converting
      */
+    if (!(req.get("Content-Type") === "application/xml")) {
+        next();
+        return;
+    }
     let newBody = {}
     try {
         for (let tag of Object.keys(req.body["data"]["array"][0])) {
@@ -21,3 +25,5 @@ function parseXML(req, res, next) {
         res.sendStatus(400);
     }
 }
+
+module.exports = {parseXML};
