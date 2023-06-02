80d307439f270571ad4d36b8d2436b4c1c188166
patrick <thordal.simetzberger@gmail.com>
2023-06-02 17:23:35 +0200

    refactored builder and created formBuilder

--- server/files/javascript/builder.js
+++ server/files/javascript/builder.js
@@ -1,39 +1,21 @@
 
-class RecipeCoverBuilder {
-    constructor(recipe) {
-        this.article = document.createElement("article");
-        this.article.appendChild(
-            this.createParagraph([this.createImage(recipe.image, "recipeImage")], "imageWrapper")
-        );
-        this.article.appendChild(
-            this.createHeadline(recipe.title, "recipeTitle")
-        );
-        this.article.appendChild(
-            this.createParagraph([
-                this.createFontAwesomeIcon("fa-star", "ratingStar"),
-                this.createSpan(this.createRatingString(recipe))
-            ], "rating")
-        );
-        this.article.appendChild(
-            this.createParagraph([
-                this.createFontAwesomeIcon("fa-clock", "clock"),
-                this.createSpan(recipe.timeToMake.toString() + " minutes", "time"),
-                this.createFontAwesomeIcon("fa-chart-bar"),
-                this.createSpan(recipe.difficulty.toString(), "difficulty")
-            ], "additionalInfo")
-        )
+class Builder {
+
+    constructor(tag) {
+        this.element = document.createElement(tag) ;
     }
 
     createRatingString(recipe) {
         return recipe.ratingStars.toString() + "/5 (" + recipe.ratingAmount.toString() + ")";
     }
 
-    createParagraph(children, clazz="", id="") {
+    createParagraph(children, clazz="", id="", textContent="") {
         const p = document.createElement("p");
         if (clazz) p.classList.add(clazz);
         if (id) p.id = id;
-        for (let element of children) {
-            p.appendChild(element);
+        if (textContent) p.textContent = textContent;
+        for (let child of children) {
+            p.appendChild(child);
         }
         return p;
     }
@@ -67,11 +49,86 @@ class RecipeCoverBuilder {
         return span;
     }
 
+    createLabel(textContent, clazz="", id="", f="") {
+        const label = document.createElement("label");
+        if (clazz) label.classList.add(clazz)
+        if (id) label.id = id;
+        if (f) label.setAttribute("for", f);
+        label.textContent = textContent;
+        return label;
+    }
+
+    createInput(type, clazz="", id="", name="", value="", required=false) {
+        const input = document.createElement("input");
+        input.type = type;
+        if (clazz) input.classList.add(clazz);
+        if (id) input.id = id;
+        if (name) input.name = name;
+        if (value) input.value = value;
+        input.required = required;
+        return input;
+    }
+
     appendTo(parent) {
-        parent.appendChild(this.article);
+        console.log(parent)
+        parent.appendChild(this.element);
+    }
+
+    addChildren(children) {
+        for (let child of children) {
+            this.element.appendChild(child);
+        }
     }
 }
+class RecipeCoverBuilder extends Builder{
+    constructor(recipe) {
+        super("article");
+        this.element.appendChild(
+            super.createParagraph([super.createImage(recipe.image, "recipeImage")], "imageWrapper")
+        );
+        this.element.appendChild(
+            super.createHeadline(recipe.title, "recipeTitle")
+        );
+        this.element.appendChild(
+            super.createParagraph([
+                super.createFontAwesomeIcon("fa-star", "ratingStar"),
+                super.createSpan(super.createRatingString(recipe))
+            ], "rating")
+        );
+        this.element.appendChild(
+            super.createParagraph([
+                super.createFontAwesomeIcon("fa-clock", "clock"),
+                super.createSpan(recipe.timeToMake.toString() + " minutes", "time"),
+                super.createFontAwesomeIcon("fa-chart-bar"),
+                super.createSpan(recipe.difficulty.toString(), "difficulty")
+            ], "additionalInfo")
+        )
+    }
 
+
+}
+
+class FormBuilder  extends Builder {
+    constructor(formObject, title, buttonValue, prefillValue={}) {
+        super("form");
+
+        this.element.appendChild(super.createParagraph([], "formTitle", "", title));
+        for (let line in formObject) {
+            if (!prefillValue.hasOwnProperty(line)) {
+                    prefillValue[line] = "";
+            }
+            super.addChildren([
+            super.createParagraph([
+                super.createLabel(line, "standardFont", "", line),
+                super.createInput(formObject[line], "standardFont", line, line, prefillValue[line], true)
+            ], "authenticationFormElement")]);
+        }
+        this.element.appendChild(super.createParagraph([
+            super.createInput("submit", "", "", "", buttonValue)
+        ], "authenticationFormElement"))
+
+    }
+}
 window.onload = function () {
     let recipeOfTheDay = {
     "title": "recipeOfTheDay",
@@ -94,6 +151,10 @@ window.onload = function () {
     "image":"../../resources/img/anonymFood.jpg",
     "difficulty":"medium"
     }
-    new RecipeCoverBuilder(recipeOfTheDay).appendTo(document.getElementById("body"));
-
+    //new RecipeCoverBuilder(recipeOfTheDay).appendTo(document.getElementById("body"));
+    let form = {
+        "username":"text",
+        "password":"password"
+    }
+    //new FormBuilder(form, "Login", "Log in", {}).appendTo(document.getElementById("test"))
 }
