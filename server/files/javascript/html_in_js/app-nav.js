dece48bbe5908012661d9c3dc0ca70d5dac5aec2
patrick <thordal.simetzberger@gmail.com>
2023-06-19 12:50:33 +0200

    added frontend check if jwt cookie is expired

--- server/files/javascript/html_in_js/app-nav.js
+++ server/files/javascript/html_in_js/app-nav.js
@@ -18,16 +18,31 @@ class AppNav extends HTMLElement {
 
 customElements.define("app-nav", AppNav);
 
+function isExpired(jwt) {
+    const payload = JSON.parse(atob(jwt));
+    const expirationTimeStamp = payload.exp;
+    const expirationDate = new Date(expirationTimeStamp * 1000);
+    const currentDate = new Date();
+    return expirationDate < currentDate;
+
+}
+
+function logout() {
+    deleteCookie("jwt");
+   deleteCookie("username");
+   deleteCookie("userID");
+}
+
 window.addEventListener("load", function() {
     let jwtCookie = getCookie("jwt")
     if (jwtCookie !== null) {
+        if (isExpired(jwtCookie)) {
+            logout();
+            return;
+        }
         document.getElementById("loginAnchor").textContent = "Logout";
         document.getElementById("user").style.display = "block";
-        document.getElementById("loginAnchor").onclick = function () {
-           deleteCookie("jwt");
-           deleteCookie("username");
-           deleteCookie("userID");
-        }
+        document.getElementById("loginAnchor").onclick = logout;
         document.getElementById("loginAnchor").href = "/";
     }
 })
