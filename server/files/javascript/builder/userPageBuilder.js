847165ad446f9f3ccb4a75f8635d63bfe2f8004a
patrick <thordal.simetzberger@gmail.com>
2023-06-26 21:05:07 +0200

    added userpage for others to see

--- server/files/javascript/builder/userPageBuilder.js
+++ server/files/javascript/builder/userPageBuilder.js
@@ -8,40 +8,71 @@ export default class userPageBuilder extends Builder{
         this.element.appendChild(
             super.createElement("p", {"class":"formTitle", textContent:"user Page:"})
         )
-        this.element.appendChild(
-            super.createElement("p", {
-                "id":"usernameParagraph",
-                "children": [
-                    super.createElement("input", {
-                        "class":"standardFont",
-                        "id":"username",
-                        "type":"text",
-                        "placeholder": data["username"],
-                        "required":true
-                    })
-                ]
-            })
-        )
-        this.element.appendChild(
-            super.createElement("p", {
-                "id":"options",
-                "children": [
-                    super.createElement("label", {"for":"optionsInput", "textContent":"view nutritional values"}),
-                    super.createElement("input", {"id":"optionsInput", "type":"checkbox"})
-                ]
-            })
-        )
-        this.element.appendChild(
-            super.createElement("input", {"class":"saveOptions", "type":"submit", "value":"save options"})
-        )
-        let pwElement = super.createElement("div", {"id":"changePasswordParagraph"})
-        this.element.appendChild(pwElement)
-        let emailElement = super.createElement("div", {"id":"changeEmail"})
-        this.element.appendChild(emailElement)
-        new FormBuilder({"password":"password", "confirm_password":"password"}, "change password:", "update Password", {}, {"baseID":"passwordForm"}).appendTo(pwElement);
-        new FormBuilder({"email":"text"}, "change email:", "update email", {"email":"testmail"}, {"baseID":"emailForm"}).appendTo(emailElement);
-        this.element.appendChild(
-            super.createElement("div", {"id":"recipeCoverWrapper"})
-        )
+        if (updateAble) {
+            let tempElement = super.createElement("input", {
+                    "id": "createRecipeButton",
+                    "class":"saveOptions",
+                    "type":"submit",
+                    "value":"create Recipe"
+                })
+            tempElement.onclick = function() {
+                window.location.href = "/recipe/createRecipe.html"
+            }
+            this.element.appendChild(
+                super.createElement("p", {
+                    "id":"usernameParagraph",
+                    "children": [
+                        super.createElement("input", {
+                            "class":"standardFont",
+                            "id":"username",
+                            "type":"text",
+                            "placeholder": data["username"],
+                            "required":true
+                        })
+                    ]
+                })
+            )
+            this.element.appendChild(tempElement)
+            this.element.appendChild(
+                super.createElement("p", {
+                    "id":"options",
+                    "children": [
+                        super.createElement("label", {"for":"optionsInput", "textContent":"view nutritional values"}),
+                        super.createElement("input", {"id":"optionsInput", "type":"checkbox"})
+                    ]
+                })
+            )
+            this.element.appendChild(
+                super.createElement("input", {"class":"saveOptions", "type":"submit", "value":"save options"})
+            )
+            let pwElement = super.createElement("div", {"id":"changePasswordParagraph"})
+            this.element.appendChild(pwElement)
+            let emailElement = super.createElement("div", {"id":"changeEmail"})
+            this.element.appendChild(emailElement)
+            new FormBuilder({"password":"password", "confirm_password":"password"}, "change password:", "update Password", {}, {"baseID":"passwordForm"}).appendTo(pwElement);
+            new FormBuilder({"email":"text"}, "change email:", "update email", {"email":"testmail"}, {"baseID":"emailForm"}).appendTo(emailElement);
+            this.element.appendChild(
+                super.createElement("div", {"id":"recipeCoverWrapper"})
+            )
+        } else {
+            this.element.appendChild(
+                super.createElement("p", {
+                    "id":"usernameParagraph",
+                    "children": [
+                        super.createElement("label", {
+                            "class":"standardFont",
+                            "id":"username",
+                            "textContent":data["username"],
+                            "type":"text",
+                        })
+                    ]
+                })
+            )
+            this.element.appendChild(
+                super.createElement("div", {"id":"recipeCoverWrapper"})
+            )
+
+        }
+
     }
 }
