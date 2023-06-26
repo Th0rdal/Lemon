90ccb4f0e14a8085bc3e0b8ab8371175dfdf6bc0
patrick <thordal.simetzberger@gmail.com>
2023-06-26 21:42:50 +0200

    recipeof the day now loads correctly

--- server/files/javascript/webpageJS/index.js
+++ server/files/javascript/webpageJS/index.js
@@ -2,36 +2,34 @@ import {RecipeCoverBuilder} from "../builder/RecipeCoverBuilder.js"
 
 
 
-fetch('/recipe/ofTheDay')
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error('Fehler beim Abrufen des zufälligen Gerichts');
-        }
-    })
-    .then(data => {
-
-        new RecipeCoverBuilder(data).appendTo(document.getElementById("recipeCoverWrapper"));
-
-    })
-    .catch(error => {
-        console.error(error);
-        // Behandle den Fehler
-    });
-
-
-// window.onload = function() {
-//     let xhr = new XMLHttpRequest();
-//     xhr.onload = function() {
-//         if (xhr.status === 200) {
-//             const body = JSON.parse(xhr.responseText)
-//             for (let index in body) {
-//                 new RecipeCoverBuilder(body[index]).appendTo(document.getElementById("recipeCoverWrapper"))
-//             }
+// fetch('/recipe/ofTheDay')
+//     .then(response => {
+//         if (response.ok) {
+//             return response.json();
+//         } else {
+//             throw new Error('Fehler beim Abrufen des zufälligen Gerichts');
 //         }
-//     }
-//     let url = "filter?amount=" + encodeURIComponent(25);
-//     xhr.open("GET", url);
-//     xhr.send();
-// }
+//     })
+//     .then(data => {
+//
+//         new RecipeCoverBuilder(data).appendTo(document.getElementById("recipeCoverWrapper"));
+//
+//     })
+//     .catch(error => {
+//         console.error(error);
+//         // Behandle den Fehler
+//     });
+
+
+window.onload = function() {
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const body = JSON.parse(xhr.responseText)
+            console.log(body)
+            new RecipeCoverBuilder(body).appendTo(document.getElementById("recipeCoverWrapper"))
+        }
+    }
+    xhr.open("GET", "recipe/ofTheDay");
+    xhr.send();
+}
